e26bc3eb1f116e1c825d58d0cb6e351775b66b3d
David Henry <dah06a@acu.edu>
2024-08-10 21:16:01 -0500

    Disabled error for temporary prototype

--- app/climateStory/page.tsx
+++ app/climateStory/page.tsx
@@ -22,22 +22,22 @@ export default function ClimateStory() {
 
 	const currStage = storyStages.find((stage) => stage.id === stageParams);
 	const options = currStage?.options.map((stage) => (
-		<li key={stage.optId} className="mx-auto w-4/5 md:w-2/3 lg:w-1/2">
-			<Suspense key={currStage?.id} fallback={<StoryStageSkeleton />}>
+		<Suspense key={currStage?.id} fallback={<StoryStageSkeleton />}>
+			<li key={stage.optId} className="mx-auto w-4/5 md:w-2/3 lg:w-1/2">
 				<button
 					className="w-full my-2 p-2 rounded bg-blue-500"
 					onClick={() => handleSelectOption(stage.to)}
 				>
 					{stage.text}
 				</button>
-			</Suspense>
-		</li>
+			</li>
+		</Suspense>
 	));
 
 	return (
-		<main className="flex flex-col min-h-screen items-center">
-			<div className="flex-1 flex flex-col items-center justify-center w-full p-4 md:p-8 lg:p-16 bg">
-				<Suspense key={currStage?.id} fallback={<StoryStageSkeleton />}>
+		<Suspense key={currStage?.id} fallback={<StoryStageSkeleton />}>
+			<main className="flex flex-col min-h-screen items-center">
+				<div className="flex-1 flex flex-col items-center justify-center w-full p-4 md:p-8 lg:p-16 bg">
 					{currStage?.mainText}
 					{currStage?.id !== "end" && (
 						<Image
@@ -48,13 +48,11 @@ export default function ClimateStory() {
 							className="pt-8"
 						/>
 					)}
-				</Suspense>
-			</div>
-			<div className="flex-2 flex items-center justify-center w-full">
-				<Suspense key={currStage?.id} fallback={<StoryStageSkeleton />}>
+				</div>
+				<div className="flex-2 flex items-center justify-center w-full">
 					<ul className="w-full">{options}</ul>
-				</Suspense>
-			</div>
-		</main>
+				</div>
+			</main>
+		</Suspense>
 	);
 }
